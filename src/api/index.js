9c7fa606285321863596afac46971f5cde71ccc8
Aandrea <aandreab8@gmail.com>
2022-08-02 10:42:17 -0700

    updated code

--- src/api/index.js
+++ src/api/index.js
@@ -1,51 +1,45 @@
-export const BASE_URL = '';
+export const BASE_URL = 'http://localhost:3000/api';
 
 
 export const registerUser = async (username, password) => {
   try {
-    const response = await fetch(`${BASE_URL}/users/register`,
-      {
-        method: "POST",
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          user: {
-            username,
-            password
-          }
-        })
-      })
-    const { data: { token } } = await response.json();
-    //console.log(token)
-    return token
+    const response = await fetch(`${BASE_URL}/users/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    });
+    const token  = await response.json();
+    console.log(token);
+    return token;
+  } catch (error) {
+    console.error("error", error);
   }
-  catch (err) {
-    console.error(err);
-  }
-}
+};
 
 export const loginUser = async (username, password) => {
   try {
-    const response = await fetch(`${BASE_URL}/users/login`,
-      {
-        method: "POST",
-        headers: {
-          'Content-Type': 'application/json'
+    const response = await fetch(`${BASE_URL}/users/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: {
+          username,
+          password,
         },
-        body: JSON.stringify({
-          user: {
-            username,
-            password
-          }
-        })
-      })
+      }),
+    });
 
-    const { data: { token } } = await response.json();
+    const token  = await response.json();
     //console.log(token)
-    return token
-  }
-  catch (err) {
+    return token;
+  } catch (err) {
     console.error(err);
   }
-}
+};

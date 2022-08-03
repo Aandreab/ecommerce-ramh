3d9448d4e07803daea047c4dd9d0ac37a68a2b82
Henry <henryqhuynh@gmail.com>
2022-08-02 19:28:27 -0700

    Adjusting key db files authors and product

--- db/product.js
+++ db/product.js
@@ -1,6 +1,58 @@
-const client = require("../client/client");
+const client = require("../client");
 
 // DATABASE FUNCTIONS
+const createProduct = async ({
+    title,
+    author,
+    publisher,
+    image,
+    genre,
+    description,
+    rating,
+    price,
+    iventory,
+    isActive = true,
+}) => {
+    try {
+        const { rows } = await client.query(
+            `
+            INSERT INTO products(title, author, publisher, image, genre, description, rating, price, iventory, isActive)
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+            RETURNING *;
+            `,
+            [
+            title,
+            author,
+            publisher,
+            image,
+            genre,
+            description,
+            rating,
+            price,
+            iventory,
+            isActive
+            ]
+        );
+        return rows;
+    } catch (error) {
+        console.error("Error: Problem creating product...", error)
+    }
+};
+
+const getProductById = async (id) => {
+    try {
+        const { rows } = await client.query(`
+        SELECT * FROM products
+        WHERE id = $1;
+        `,
+        [id]
+        );
+        return rows;
+    } catch (error) {
+    console.error("Error: Problem getting products by Id...", error)
+    }
+};
+
 async function getAllProducts() {
     try {
         const { rows } = await client.query(
@@ -11,21 +63,6 @@ async function getAllProducts() {
         return rows;
     } catch (error) {
         console.log("Error: Problem retrieving all products...", error);
-    }
-}
-
-async function getProductById(id) {
-    try {
-        const {
-            rows: [product],
-        } = await client.query(`
-      SELECT * FROM products
-      WHERE id = $1;
-      `, [id]);
-        console.log(product)
-        return product;
-    } catch (error) {
-        console.error("Error: Problem getting products by Id...", error);
     }
 }
 
@@ -44,35 +81,7 @@ async function getProductByName(name) {
     }
 }
 
-async function createProduct({
-    name,
-    description,
-    photoUrl,
-    authorId,
-    distId,
-    department,
-    price,
-    inStock,
-    quantity,
-    count,
-}) {
-    try {
-        const {
-            rows: [product],
-        } = await client.query(
-            `
-        INSERT INTO products(name, description, "photoUrl", authorId, distId, quantity, price, department, "inStock",  count)
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
-        RETURNING *;
-      `,
-            [name, description, photoUrl, authorId, distId, quantity, price, department, inStock, count]
-        );
 
-        return product;
-    } catch (error) {
-        console.error("Error: Problem creating product...")
-    }
-}
 
 async function updateProduct({ id, description, authorId, distId }) {
     const fields = { description, authorId, distId };

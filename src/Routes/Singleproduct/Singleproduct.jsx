fd3cf1ec8a380e3135211e1fc324655c2e5ec2ce
Aandrea <aandreab8@gmail.com>
2022-08-02 17:31:59 -0700

    created fake data json file and began cart and single item functionality

--- src/Routes/Singleproduct/Singleproduct.jsx
+++ src/Routes/Singleproduct/Singleproduct.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
 import './Singleproduct.css'
 
-export default function Singleproduct() {
+export default function Singleproduct({name, price, image}) {
     return (
-      <main className='singleproduct-container'>
-       
-      </main>
+     <Card>
+       <Card.Img variant = "top" src={image}/>
+       <Card.Body>
+         <Card.Title>
+           <div>{name}</div>
+           <div>{price}</div>
+         </Card.Title>
+       </Card.Body>
+     </Card>
     );
   }

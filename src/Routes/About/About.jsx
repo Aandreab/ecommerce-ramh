c023d1fe090178210d6dfba4ddd018f9d1c53ddd
Aandrea <aandreab8@gmail.com>
2022-08-11 16:30:56 -0700

    displays user cart, removed old backend code, added privacy policy, fixed footer

--- src/Routes/About/About.jsx
+++ src/Routes/About/About.jsx
@@ -15,7 +15,7 @@ export default function About() {
             <Col className="about-col" xs={6}>
               <img
                 className="d-block w-100"
-                src={images.ryan}
+                src={images.tempimg}
                 alt="photograph"
               />
             </Col>
@@ -33,7 +33,7 @@ export default function About() {
             <Col className="about-col" xs={6}>
               <img
                 className="d-block w-100"
-                src={images.aandrea}
+                src={images.tempimg}
                 alt="photograph"
               />
             </Col>
@@ -48,7 +48,7 @@ export default function About() {
             </Col>
             <Col xs={6}>
               <h1 className="about-heading">HENRY HUYNH</h1>
-              <h1 className="about-description">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur</h1>
+              <h1 className="about-description">Hi there, I'm Henry Huynh. I'm an ambitious, reliable, and coachable tech enthusiast and full-stack developer. My interests have always had me exploring the tech world and have had me branching out to improve and strengthen my skills. This past year, I decided to take a strong step forward to improve my understanding of technology and software development. So I took a full-stack coding program focused on Front-End and Back-End development learning HTML, CSS, JS, and REACT. I focused on the backend portion of this website designing its database and api routes. There is room for improvement, but I am quite proud of the work my team has put forth.</h1>
             </Col>
           </Row>
 
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,29 +11,55 @@ import { AiFillTwitterCircle } from 'react-icons/ai';
 import { AiFillFacebook } from 'react-icons/ai';
 import { AiFillInstagram } from 'react-icons/ai';
 import { AiFillLinkedin } from 'react-icons/ai';
+import {useState} from 'react';
+import { useNavigate } from "react-router-dom";
 export default function Footer() {
+  const [email, setEmail] = useState('');
+  const navigate = useNavigate();
+  const handleChange = event => {
+    setEmail(event.target.value);
+  };
+  const handleClick = () => {
+    setEmail('');
+  };
+  const handleNewsletter = async (e) => {
+    e.preventDefault();
+    navigate("/Newsletter")
+  }
+  const handleProducts = async (e) => {
+    e.preventDefault();
+    navigate("/Products")
+  }
+  const handleAbout = async (e) => {
+    e.preventDefault();
+    navigate("/About")
+  }
+  const handlePrivacy = async (e) => {
+    e.preventDefault();
+    navigate("/PrivacyPolicy")
+  }
   return (
     <footer className='footer'>
       <Container className="footer-container">
         <Row className="footer-row">
           <Col className="footer-col">
-          <div className='footer-company-header'>KAMIKAZE COMICS</div>
-          <div className='footer-company-desc'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehender</div>
+          <div className='footer-company-header'>CometKaze Comics</div>
+          <div className='footer-company-desc'>Welcome to CometKaze Comics! Your one stop shop for all things comic related. We pride ourselves on a robust base of comic titles and categories. Thank you for your interest in our site and we look forward to providing you with the highest quality comics in a timely manner. Now get out there and get your Comic(on)!</div>
           </Col>
 
           <Col className="footer-col">
           <div className='quicklinks-header'>QUICK LINKS</div>
-          <div className='footer-links'>Comics</div>
-          <div className='footer-links'>Newsletter</div>
-          <div className='footer-links'> About Us </div>
-          <div className='footer-links'>Privacy Policy</div>
+          <div className='footer-links' onClick={handleProducts}>Comics</div>
+          <div className='footer-links'onClick={handleNewsletter}>Newsletter</div>
+          <div className='footer-links' onClick={handleAbout}> About Us </div>
+          <div className='footer-links' onClick={handlePrivacy}>Privacy Policy</div>
           </Col>
 
           <Col className="footer-col">
             <h1 className='footer-sign-up-header'> SIGN UP FOR OUR NEWSLETTER</h1>
             <InputGroup className="mb-3">
-              <Form.Control placeholder="Email" />
-              <Button variant="outline-secondary" id="button-addon2">
+              <Form.Control placeholder="Email" type="text" onChange={handleChange} value={email}/>
+              <Button onClick={handleClick} variant="outline-secondary" id="button-addon2">
                 <BsFillCursorFill className='curser' />
               </Button>
             </InputGroup>
@@ -47,7 +73,7 @@ export default function Footer() {
 
         </Row>
         <Row>
-          <p className="copyright">Kamikaze Comics Copyright © 2022</p>
+          <p className="copyright">CometKaze Comics Copyright © 2022</p>
         </Row>
       </Container>
     </footer>
